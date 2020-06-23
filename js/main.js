ba252cd00f348a5a1bbcca892ec1d38ec284fd4b
Tiago Ssa <tiago.silva301@gmail.com>
2020-06-23 14:35:32 +0100

    Criar function consultacep - ajax

---
+++ js/main.js
@@ -0,0 +1,25 @@
+function consultaCep(){
+    var cep = document.getElementById('cep').value;
+    var url = 'https://viacep.com.br/ws/' + cep + '/json/';
+
+    console.log(url);
+
+
+    $.ajax({
+        url: url, // ou 'https://viacep.com.br/ws/40130030/json/',
+        type:'GET',
+        success: function(response){
+            console.log(response);
+          /*document.getElementById('logradouro').innerHTML = response.logradouro;
+            document.getElementById('bairro').innerHTML = response.bairro;
+            document.getElementById('localidade').innerHTML = response.localidade;
+            document.getElementById('uf').innerHTML = response.uf; */
+
+            //shortest path
+            $('#logradouro').html(response.logradouro);
+            $('#bairro').html(response.bairro);
+            $('#localidade').html(response.localidade);
+            $('#uf').html(response.uf);
+        }
+    })
+}
